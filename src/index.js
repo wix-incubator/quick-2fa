0250acbe0b81907e963ed6da7232a3f4815163c2
Shahar Talmi <shahart@wix.com>
2017-01-27 23:58:09 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const speakeasy = require('speakeasy');
+const notifier = require('node-notifier');
+const ncp = require('copy-paste');
+
+if (!process.argv[2]) {
+  console.error('Usage: quick-2fa YOUR-KEY');
+  process.exit(1);
+}
+
+const token = speakeasy.totp({
+  secret: process.argv[2],
+  encoding: 'base32'
+});
+
+ncp.copy(token);
+
+notifier.notify({
+  title: 'quick-2fa',
+  message: token
+});
+
+console.log(token);
